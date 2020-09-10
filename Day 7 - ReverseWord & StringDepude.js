7007a454fbce5dd18b84f094d770ae8437cdea60
Shivani Autar <shivaniautar18@gmail.com>
2020-09-09 22:03:54 -0700

    Day7

---
+++ Day 7 - ReverseWord & StringDepude.js
@@ -0,0 +1,43 @@
+/*
+  Given a string,
+  return a new string with the duplicates excluded
+  Bonus: Keep only the last instance of each character.
+*/
+
+const str1 = "abcABC"
+// const expected1 = "abcABC"
+
+const str2 = "helloo"
+// const expected2 = "helo"
+
+function stringDedupe(str) {
+
+}
+
+console.log(stringDedupe(str1));
+
+console.log(stringDedupe(str2));
+
+/* ******************************************************************************** */
+
+/*
+  Given a string containing space separated words
+  Reverse each word in the string.
+  If you need to, use .split to start, then try to do it without.
+*/
+
+const str1 = "hello"
+// const expected1 = "olleh"
+
+const str2 = "hello world"
+// const expected2 = "olleh dlrow"
+
+const str3 = "abc def ghi"
+// const expected3 = "cba fed ihg"
+
+function reverseWords(str) {
+
+}
+console.log(reverseWords(str1));
+console.log(reverseWords(str2));
+console.log(reverseWords(str3));
