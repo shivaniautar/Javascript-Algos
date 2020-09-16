2763fe63fb764e8f0e3d4c6205b797ff4689f3fb
Shivani Autar <shivaniautar18@gmail.com>
2020-09-15 22:27:10 -0700

    Day11

--- Day 11 - BinarySearch (non-recursive).js
+++ Day 11 - BinarySearch (non-recursive).js
@@ -21,11 +21,57 @@ const nums3 = [3, 4, 6, 8, 12]
 const searchNum3 = 3
 const expected3 = true
 
+function binarySearch(sortedNums, searchNum) {
+    var middleNum = Math.floor((sortedNums.length)/2);
+    if(searchNum<=sortedNums[middleNum]){
+        for(var i=0;i<=middleNum;i++){
+            if(sortedNums[i] == searchNum){
+                return true;
+            }
+        }
+    }
+    if(searchNum>sortedNums[middleNum]){
+        for(var j=middleNum;j<=sortedNums.length-1;j++){
+            if(sortedNums[j] == searchNum){
+                return true;
+            }
+        }
+    }
+  return false
+}
+console.log(binarySearch(nums1, searchNum1));
+console.log(binarySearch(nums2, searchNum2));
+console.log(binarySearch(nums3, searchNum3));
+console.log(binarySearch(nums4, searchNum4));
+
+
+
 // bonus, how many times does the search num appear?
 const nums4 = [2, 2, 2, 2, 3, 4, 5, 6, 7, 8, 9]
 const searchNum4 = 2
 const expected4 = 4
 
 function binarySearch(sortedNums, searchNum) {
-  // code here
+    var count=0;
+    var middleNum = Math.floor((sortedNums.length)/2);
+    if(searchNum<=sortedNums[middleNum]){
+        for(var i=0;i<=middleNum;i++){
+            if(sortedNums[i] == searchNum){
+                count= count+1;
+                continue;
+            }
+          return count;
+        }
+    }
+    if(searchNum>sortedNums[middleNum]){
+        for(var j=middleNum;j<=sortedNums.length-1;j++){
+            if(sortedNums[j] == searchNum){
+                count=count+1;
+                continue;
+            }
+        return count;
+        }
+    }
+  return count;
 }
+console.log(binarySearch(nums4, searchNum4));
