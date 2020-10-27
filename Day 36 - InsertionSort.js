8c6e53f2b07287dcda6063d1bfe9916fd0ebff31
Shivani Autar <shivaniautar18@gmail.com>
2020-10-27 16:38:30 -0700

    Day36

---
+++ Day 36 - InsertionSort.js
@@ -0,0 +1,32 @@
+/**
+ * takes in an array
+ * returns THE SAME array sorted
+ * iterate through each element starting with the second
+ * work backwards to "insert" the element
+ * in the correct position
+ * elements get duplicated forward to make way for the
+ * inserted element
+ */
+
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        let insertionIndex = i;
+        if (arr[i] < arr[i-1]){
+            for (let j = i-1; j >= 0; j--) {
+                if (arr[i] < arr[j]) {
+                    insertionIndex = j;
+                }
+            }
+        }
+        if (insertionIndex != i) {
+            let temp = arr[i];
+            for (let k = i; k > insertionIndex; k--) {
+                arr[k] = arr[k-1];
+            }
+            arr[insertionIndex] = temp;
+        }
+    }
+    return arr;
+}
+console.log(insertionSort([1, 5, 2, 8, 3, 4]));
+// should log [1, 2, 3, 4, 5, 8]
