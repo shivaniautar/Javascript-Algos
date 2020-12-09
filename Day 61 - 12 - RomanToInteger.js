6e438a96a305fe9ebbf7a31fc4d2d2ea1ee1ac3f
Shivani Autar <shivaniautar18@gmail.com>
2020-12-08 21:31:47 -0800

    Day61

--- Day 61 - 12 - RomanToInteger.js
+++ Day 61 - 12 - RomanToInteger.js
@@ -27,6 +27,25 @@
 
 // Input: s = "IV"  
 
+// Example 2:
+
+// Input: s = "IV"
+// Output: 4
+// Example 3:
+
+// Input: s = "IX"
+// Output: 9
+// Example 4:
+
+// Input: s = "LVIII"
+// Output: 58
+// Explanation: L = 50, V= 5, III = 3.
+// Example 5:
+
+// Input: s = "MCMXCIV"
+// Output: 1994
+// Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
 
 
 
