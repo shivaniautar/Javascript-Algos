a3fe3f452f9a99a22beddb025fd5ce75912045d4
Shivani Autar <shivaniautar18@gmail.com>
2020-12-10 21:49:50 -0800

    Day63

--- Day 63 - 14 - MergeTwoSortedLists.js
+++ Day 63 - 14 - MergeTwoSortedLists.js
@@ -25,4 +25,36 @@
 
 
 
-
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+// /**
+//  * @param {ListNode} l1
+//  * @param {ListNode} l2
+//  * @return {ListNode}
+//  */
+var mergeTwoLists = function(l1, l2) {
+    let x = new ListNode(-1);
+    let head = x;
+    
+    while (l1 !== null && l2 !== null){
+        if(l1.val <= l2.val){
+            x.next = l1;
+            l1 = l1.next;
+        } else{
+            x.next = l2;
+            l2 = l2.next;
+        }
+        x = x.next;
+    }
+    if(l1 !== null){
+        x.next =l1;
+    } else{
+        x.next =l2;
+    }
+    return head.next;
+};
