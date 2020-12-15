c105d47bb3330bb3ce149285024e6e8e662fc0ce
Shivani Autar <shivaniautar18@gmail.com>
2020-12-14 21:44:40 -0800

    PlusOne

--- Day 64 - 15 - PlusOne.js
+++ Day 64 - 15 - PlusOne.js
@@ -26,3 +26,23 @@
 
 // 1 <= digits.length <= 100
 // 0 <= digits[i] <= 9
+
+
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+
+    for(let i=digits.length-1;i>=0;i--){
+        digits[i]++;
+        if(digits[i]<10){
+            return digits;
+        } else{//49
+            digits[i]=0;
+        }
+   }
+   //99->100
+   digits.unshift(1)
+   return digits;
+};
