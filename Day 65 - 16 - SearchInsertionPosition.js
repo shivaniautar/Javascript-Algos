4457a54148b1ef4bb78420d5de1ada7ca6b5ed83
Shivani Autar <shivaniautar18@gmail.com>
2020-12-16 20:14:33 -0800

    Sln

--- Day 65 - 16 - SearchInsertionPosition.js
+++ Day 65 - 16 - SearchInsertionPosition.js
@@ -30,3 +30,24 @@
 // -104 <= nums[i] <= 104
 // nums contains distinct values sorted in ascending order.
 // -104 <= target <= 104
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let i=0;
+    let j= nums.length;
+    while(i<=j){
+        const middle = Math.floor((i+j)/2)
+        if(nums[middle] === target){
+            return middle;
+        } else if(nums[middle]<target){
+            i+=1
+        } else{
+            j-=1
+        }
+    }
+    return i;
+};
