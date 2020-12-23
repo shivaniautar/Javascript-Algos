19ee1252fc10726e2dc46e6b07f35d764d724d6e
Shivani Autar <shivaniautar18@gmail.com>
2020-12-22 21:03:11 -0800

    solution

--- Day 67 - 18 - CountAndSay.js
+++ Day 67 - 18 - CountAndSay.js
@@ -39,5 +39,36 @@
  * @return {string}
  */
 var countAndSay = function(n) {
+
+
+
+    //O(n) solution where n is the input
+    //Solution that goes digit by digit, and if a distinct digit is passed,
+    //we add the saying to the solution
     
-};
+    let temp = '1'
+        
+    for (let i = 1; i < n; i++) {
+        let arr = temp.split('')
+        
+        //Make it empty and fill it w/ next iteration
+        temp = ''
+        let count = 1
+        
+        for (let j = 0; j < arr.length; j++) {
+            //If adjacent characters don't match, add the 'saying' to the answer
+            if (arr[j] != arr[j + 1]) {
+                temp += count + arr[j]
+                count = 1
+            //Else if they do match, move to the next character and increment the count
+            } else {
+                count++
+            }
+        }
+    }
+    
+    return temp
+        
+    };
+    
+    
