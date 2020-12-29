b5580aae7b222189939d8bddd69cf64e9455de5f
Shivani Autar <shivaniautar18@gmail.com>
2020-12-28 21:47:13 -0800

    LongestCommonPrefix

--- Day 69 - 20 - LongestCommonPreffix.js
+++ Day 69 - 20 - LongestCommonPreffix.js
@@ -22,3 +22,30 @@
 // 0 <= strs.length <= 200
 // 0 <= strs[i].length <= 200
 // strs[i] consists of only lower-case English letters.
+
+//Objective is to find the longest common prefix between words in a list.
+
+let strs = ["flower","flow","flight"]
+
+
+//O(n) solution that continuously modifies the prefix until it's the prefix
+//of every word.
+
+if (strs.length == 0) {
+    return ''
+}
+
+let prefix = strs[0]
+
+for (let string of strs) {
+    //As long as it's not the prefix, keep cutting off characters from the prefix.
+    while (string.indexOf(prefix) !== 0) {
+        prefix = prefix.substring(0, prefix.length - 1)
+    }
+    
+    if (prefix.length == 0) {
+        return ''
+    }
+}
+
+return prefix
